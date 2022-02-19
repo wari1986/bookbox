403ddae863f26fe1b3afe62ad3e0268a2509c9d6
aadel bouzambou <abouzambou@gmail.com>
2022-02-19 15:55:22 +0100

    accept the incomming change

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,4 @@
 class BooksController < ApplicationController
-<<<<<<< HEAD
-=======
   def index
     @books = Book.all
     # @markers = [{
@@ -15,20 +13,11 @@ class BooksController < ApplicationController
 
   end
 
->>>>>>> 738a5c8fa2ba8228cbb44ffde44752f31cbbd103
   def new
     @book = Book.new
   end
 
   def create
-<<<<<<< HEAD
-    @book = Book.new(params[:book])
-    @book.save
-  end
-
-  def book_params
-    params.require(:book).permit(:condition, :location, :cover, :title, :author, :category, :year, :descritpion, :language)
-=======
     @book = Book.new(book_params)
     @book.user = current_user
    if @book.save
@@ -43,6 +32,5 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :condition, :location, :author, :category, :year, :address, :cover, :description, :language, :credit_worth)
->>>>>>> 738a5c8fa2ba8228cbb44ffde44752f31cbbd103
   end
 end
