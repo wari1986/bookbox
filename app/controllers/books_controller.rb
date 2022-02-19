8e6f8ae5c2071f5afd7941baa65998fefff54887
aadel bouzambou <abouzambou@gmail.com>
2022-02-19 15:50:38 +0100

    done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,14 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(params[:book])
+    @book.save
+  end
+
+  def book_params
+    params.require(:book).permit(:condition, :location, :cover, :title, :author, :category, :year, :descritpion, :language)
+  end
 end
