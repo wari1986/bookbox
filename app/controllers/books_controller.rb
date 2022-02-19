f478d9f5e39326627f6f5518f47446ad095eabae
wari <nicolaycamacho@gmail.com>
2022-02-19 14:52:39 +0100

    index and ahow actions created, index and show views created, books seeded

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,36 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    # @markers = [{
+    #   lat: @book.latitude,
+    #   lng: @book.longitude
+    # }]
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    # @renting = Renting.new
+
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user = current_user
+   if @book.save
+      redirect_to root_path
+   else
+    render :new
+   end
+
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :condition, :location, :author, :category, :year, :address, :cover, :description, :language, :credit_worth)
+  end
 end
