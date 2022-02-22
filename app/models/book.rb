4ea5e45128e340a381beb681a5106dd11b76ec3c
wari <nicolaycamacho@gmail.com>
2022-02-22 20:30:58 +0100

    search form done

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
-
+  has_many :books
   validates :title, :cover, :location, :condition, :category, :year, :author, :description, :language, presence: true
-
   has_one_attached :cover
-
 end
