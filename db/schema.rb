f9973ab9c8c055f895a1e5242c95919048f248ee
aadel bouzambou <abouzambou@gmail.com>
2022-02-26 15:51:01 +0100

    add new column named owned to user book relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_26_144413) do
+ActiveRecord::Schema.define(version: 2022_02_26_144905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2022_02_26_144413) do
   create_table "user_book_relationships", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "book_id", null: false
+    t.boolean "owned", default: false
     t.index ["book_id"], name: "index_user_book_relationships_on_book_id"
     t.index ["user_id"], name: "index_user_book_relationships_on_user_id"
   end
