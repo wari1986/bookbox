c9070894efcf981754ba53b9d41b766be59ea5d7
aadel bouzambou <abouzambou@gmail.com>
2022-02-26 15:00:41 +0100

    defined associations classes

--- app/models/swap.rb
+++ app/models/swap.rb
@@ -1,4 +1,6 @@
 class Swap < ApplicationRecord
   belongs_to :user
   belongs_to :book
+  belongs_to :swapper, class_name: 'User', foreign_key: :swapper_id
+  belongs_to :swapped_book, class_name: 'Book', foreign_key: :swapped_book_id
 end
