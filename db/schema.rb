1f27956fc0391e764af4a4929f34104be4c1f8eb
aadel bouzambou <abouzambou@gmail.com>
2022-02-26 15:47:42 +0100

    add new column to saps table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_26_134515) do
+ActiveRecord::Schema.define(version: 2022_02_26_144413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2022_02_26_134515) do
     t.integer "swapped_book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "accepted", default: false
     t.index ["book_id"], name: "index_swaps_on_book_id"
     t.index ["user_id"], name: "index_swaps_on_user_id"
   end
