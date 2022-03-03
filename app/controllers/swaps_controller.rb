324e08233f8990e30e83e2c72be3a50cabb9d10f
Stephan Dierckx <stephan.dierckx@hotmail.com>
2022-03-03 12:36:32 +0100

    added accept and decline actions and updated the create book action

--- app/controllers/swaps_controller.rb
+++ app/controllers/swaps_controller.rb
@@ -1,4 +1,15 @@
 class SwapsController < ApplicationController
+  def decline
+    @swap = Swap.find(params[:id])
+    @swap.accepted = false
+    @swap.save
+    redirect_to dashboard_path
+  end
 
-
+  def accept
+    @swap = Swap.find(params[:id])
+    @swap.accepted = true
+    @swap.save
+    redirect_to book_path(@swap.swapped_book_id)
+  end
 end
