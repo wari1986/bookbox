952d089b3d38c74e2e8780523f39c81f671013e4
Stephan Dierckx <stephan.dierckx@hotmail.com>
2022-03-12 14:31:51 +0100

    installed stimulus and first setup

---
+++ app/javascript/controllers/books_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  connect() {
+    console.log('hello');
+  }
+
+  fetchBooks() {
+    debugger;
+    console.log('hello');
+  }
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
